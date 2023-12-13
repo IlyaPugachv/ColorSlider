ceabc094d11360a7406b82e133e442b3d79bc502
Ilya Pugachev <ilyapugachv@gmail.com>
2023-12-13 15:59:35 +0300

    Version 1.2 HomeWork completed

--- ColorApp/ColorApp/ColorSlider.swift
+++ ColorApp/ColorApp/ColorSlider.swift
@@ -11,15 +11,22 @@ struct ColorSlider: View {
     var body: some View {
         HStack {
             Text("\(lround(sliderValue))")
-            Slider(value: $sliderValue, in: 0...100)
-            
+            Slider(value: $sliderValue, in: 0...100, step: 1)
                 .accentColor(accentColor)
             
-            TextField("", text: $textFeild)
+            TextField("", text: $textFeild, onCommit: updateSliderValue)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .frame(width: 50)
+                .keyboardType(.numberPad)
                 .padding(.leading, 10)
         }
         .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
     }
+    
+    func updateSliderValue() {
+        if let number = Double(textFeild) {
+            sliderValue = number
+        }
+    }
 }
+
--- ColorApp/ColorApp/ContentView.swift
+++ ColorApp/ColorApp/ContentView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    
     @State private var sliderValueOne: Double = 50.0
     @State private var sliderValueTwo: Double = 50.0
     @State private var sliderValueThree: Double = 50.0
@@ -10,39 +10,55 @@ struct ContentView: View {
     @State private var textFieldTwo: String = ""
     @State private var textFieldThree: String = ""
     
+    @State private var backgroundColor: Color = .white
+    
     var body: some View {
-        
-        VStack {
+        ZStack {
+            backgroundColor
             
-            ColorRectangle(color: Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100), width: 350, height: 100)
-                .cornerRadius(30)
-            
-            ColorRectangle(color: Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100), width: 350, height: 100)
-                .cornerRadius(30)
-                .padding(10)
-            
-            ColorSlider(sliderValue: $sliderValueOne, textFeild: $textFieldOne, color: .white, accentColor: .red)
-          
-            
-            ColorSlider(sliderValue: $sliderValueTwo, textFeild: $textFieldTwo, color: .white, accentColor: .green)
-            
-            ColorSlider(sliderValue: $sliderValueThree, textFeild: $textFieldThree, color: .white, accentColor: .blue)
-            
-            Button(action: {
+            VStack {
+                ColorRectangle(color: Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100), width: 350, height: 100)
+                    .cornerRadius(30)
                 
-            }) {
-                Text("Save")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                ColorRectangle(color: Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100), width: 350, height: 100)
+                    .cornerRadius(30)
+                    .padding(10)
+                
+                ColorSlider(sliderValue: $sliderValueOne, textFeild: $textFieldOne, color: .white, accentColor: .red)
+                
+                ColorSlider(sliderValue: $sliderValueTwo, textFeild: $textFieldTwo, color: .white, accentColor: .green)
+                
+                ColorSlider(sliderValue: $sliderValueThree, textFeild: $textFieldThree, color: .white, accentColor: .blue)
+                
+                Button(action: {
+                    backgroundColor = Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100)
+                }) {
+                    Text("Save")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+        }.onReceive(Just(textFieldOne)) { value in
+            if let number = Double(value) {
+                sliderValueOne = number
+                backgroundColor = Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100)
+            }
+        }.onReceive(Just(textFieldTwo)) { value in
+            if let number = Double(value) {
+                sliderValueTwo = number
+                backgroundColor = Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100)
+            }
+        }.onReceive(Just(textFieldThree)) { value in
+            if let number = Double(value) {
+                sliderValueThree = number
+                backgroundColor = Color(red: sliderValueOne / 100, green: sliderValueTwo / 100, blue: sliderValueThree / 100)
             }
         }
     }
-    
 }
 
 #Preview {
     ContentView()
-    
 }
