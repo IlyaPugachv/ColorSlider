191ec8302b73ec7bd4385c57d87cb4ad3175c98a
Ilya Pugachev <ilyapugachv@gmail.com>
2023-12-13 12:33:13 +0300

    Project created

---
+++ ColorApp/ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Илья Пугачёв on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
